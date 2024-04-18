f755fb472b457eed2a399f815830082e9c8a602d
ismailxonov <ismailxonovsiratulloh@gmail.com>
2024-04-18 07:53:23 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let a = prompt("Ismingizni kiriting");
+let b = prompt("Space id kiriting");
+let c = prompt("Pasword kiriting")
+const mars = {
+    name: a,
+    id: b,
+    pasword: c
+}
+console.log(mars);
